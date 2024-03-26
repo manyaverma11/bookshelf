b9422872b06c072c561172ddf67b9d848d568cd7
Manya <manyaverma0154@gmail.com>
2024-03-26 19:52:19 +0530

    sliding myCart, field desc, structures

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,32 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function App() {
+
+  const [count, setCount] = useState(0);
+  const [toggle, setToggle]= useState(false)
+  const [width, setWidth]= useState('')
+
+  const showMenu = () => {
+    setToggle(!toggle)
+    if(toggle === true) {
+      setWidth('16%')
+    }else {
+      setWidth('0px')
+    }
+  }
+  
 const Products = (props) => {
   const { img, title, price } = props.products
 
   return (
     <article className="products">
       <img src={img} alt={title} />
-      <div classNmae='desc'>
+      <div className='desc'>
       <h2>{title}</h2>
       <h4>{price} </h4>
       </div>
-      <button className="cartButton">Add to Cart</button>
+      <button className="cartButton" onClick={() => setCount(count + 1)}>Add to Cart</button>
     </article>
   )
 }
@@ -47,21 +62,21 @@ const products = [
   
 ]
 
-function App() {
-
   return(
     <section>
       <div className="navBar">
-        <div className="logo">PRACTICE-PROJECT</div>
+        <div className="logo">Bookshelf.in</div>
         <div className="search">
         <div className="searchBar">
-          <input type="text" name="" id="searchInput" />
+          <input type="text" name="searchingArea" id="searchInput" placeholder="Search any book" />
         </div>
-        <button className="searchBarIcon">glass</button>
+        <button className="searchBarIcon">
+          <img id="searchBarIconPhoto" src="\images\searchGlass.png"></img>
+        </button>
         </div>
         <div className="profile">
           <div className="profileName">ProfileNAme</div>
-          <button className="profilePic">ProfilePic</button>
+          <button className="myCart" onClick={showMenu}>My Cart ({count})</button>
         </div>
       </div>
       <main>
@@ -78,7 +93,13 @@ function App() {
                 )
            })}
         </div>
-        <div className="rightSideBar">hola</div>
+        <div className="rightSideBar" style={{width}}>
+          <div className='cartTop'>
+            <div className='cartHeading'>Cart</div>
+            <img src="\images\cart_symbol.png" className='cartSymbol'></img>
+            <div  className='cartCount'>({count})</div>
+          </div>
+        </div>
       </main>
     </section>
   )
